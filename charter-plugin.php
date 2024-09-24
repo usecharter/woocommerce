e44590628f966e7c04898a170ee6c43ee312a09e
cvsouth <contact@colinsouth.com>
2024-09-24 03:21:17 +0100

    ...

--- charter-plugin.php
+++ charter-plugin.php
@@ -16,11 +16,11 @@ require_once plugin_dir_path( __FILE__ ) . 'admin/menu.php';
 require_once plugin_dir_path( __FILE__ ) . 'admin/register-api-token-setting.php';
 require_once plugin_dir_path( __FILE__ ) . 'admin/page.php';
 
-// Pixel / ad click tracking
+// Record ad clicks
 require_once plugin_dir_path( __FILE__ ) . 'pixel/sync-session-uuids.php';
 require_once plugin_dir_path( __FILE__ ) . 'pixel/add-script.php';
 require_once plugin_dir_path( __FILE__ ) . 'pixel/endpoints/get-basket-uuid.php';
 require_once plugin_dir_path( __FILE__ ) . 'pixel/endpoints/set-basket-uuid.php';
 
-// Conversion recording
+// Record conversions
 require_once plugin_dir_path( __FILE__ ) . 'conversion/record-conversion.php';
