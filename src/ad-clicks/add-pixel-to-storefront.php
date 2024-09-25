4ea7ca00d66525adf4ca0876c81fe967a6fc9862
cvsouth <contact@colinsouth.com>
2024-09-25 16:46:18 +0100

    ...

--- src/ad-clicks/add-pixel-to-storefront.php
+++ src/ad-clicks/add-pixel-to-storefront.php
@@ -1,7 +1,7 @@
 <?php
 
 function charter_enqueue_script(): void {
-	wp_enqueue_script('charter-pixel', 'https://localhost:5173/pixel.js', [], '1.0', true);
+	wp_enqueue_script('charter-pixel', 'https://usecharter.io/pixel.js', [], '1.0', true);
 
 	wp_localize_script('charter-pixel', 'charter', [
 		'url' => admin_url('admin-ajax.php'),
